484399e13ec7929fa82eabdaefbde31d359358a1
Kenny Yoon <ghun.yoon@gmail.com>
2021-06-19 23:30:15 -0400

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const renderActors = actors.map(actor => {
+    return (
+      <div>
+        <h2>{actor.name}</h2>
+        <ul>
+          {actor.movies.map(movie=>{
+            return <li>{movie}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {renderActors}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,19 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const renderMovies = movies.map(movie=>{
+    return(<div>
+      <h2>{movie.title}</h2>
+      <h3>Time: {movie.time}</h3>
+      <h4>Genres: {movie.genres}</h4>
+    </div>)
+  })
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {renderMovies}
+    </div>
+  )
 }
 
 export default Movies;
